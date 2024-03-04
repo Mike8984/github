1b52ca99fe00f30b5c8cdfecc6f107b44948e444
Mike <62895017+Mike8984@users.noreply.github.com>
2024-03-04 19:11:36 +0200

    Update script.js
    changed addEventListener

--- js/script.js
+++ js/script.js
@@ -2,5 +2,5 @@ const button = document.querySelector('button')
 const heading = document.getElementById('heading')
 
 button.addEventListener('click', () => {
-  heading.textContent = 'Clicked'
+  heading.textContent = 'Hello Git'
 })
